907253bfc7ab01d3a31fd7d08c61b126087b6a0c
student <lola_gaevskaya@mail.ru>
2022-11-26 10:50:41 +0300

    commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask import url_for, render_template, redirect
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'very_secret_key'
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+@app.route("/")
+@app.route("/index")
+def index():
+    username = "Ученик емае"
+    return render_template("index.html", title="домашняя страница", username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/image_sample')
+def image():
+    return f'''<img src="{url_for('static', filename='img/111.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">'''
+
+@app.route('/sample_page')
+def return_sample_page():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Колледж!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Колледж!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
+
+@app.route("/success")
+def success():
+    return "успешный вход"
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
